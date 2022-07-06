52038aac985241bef101bf278d8e00cdec15c1c5
Etudiant BD <ebd@localhost.localdomain>
2022-07-06 16:11:28 -0400

    connection

---
+++ DBConnexion.php
@@ -0,0 +1,3 @@
+<?php
+$conn = oci_connect('c##RODIO78', 'BD1245', 'fsg-p-ora01.fsg.ulaval.ca:1521/ora19c.fsg.ulaval.ca', 'AL32UTF8');
+?>
--- pages/connexion.php
+++ pages/connexion.php
@@ -1,10 +1,32 @@
-<!Doctype html>
-<html>
-<head>
-<title>IFT-2004-TP3</title>
-</head>
-<body>
 <?php
+if(isset($_POST["SeConnecter"]))
+{
+    $email=$_POST["email"];
+    $password=$_POST["password"];
+    include("DBConnexion.php");
+    echo $conn;
+}
 ?>
-</body>
-</html>
+<div id="connexionStyle">
+	<form id="connexionStyleForm" method="post">
+		<input
+			type="text"
+			placeholder="Email"
+			name="email"
+		/>
+		<input 
+			type="password"
+			placeholder="Mot de passe"
+			name="password"
+		/>
+		<center> 
+			
+		<input 
+			type="submit"
+			value="Se connecter"
+			name="SeConnecter"
+		/>
+		</center>
+	</form>
+</div>
+	
