d0e062edf97b5f068ca8dce902938551d2428912
Etudiant BD <ebd@localhost.localdomain>
2022-07-13 21:55:33 -0400

    test

--- DBConnexion.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn = oci_connect('c##RODIO78', 'BD1245', 'fsg-p-ora01.fsg.ulaval.ca:1521/ora19c.fsg.ulaval.ca', 'AL32UTF8');
-?>
--- index.php
+++ index.php
@@ -5,19 +5,14 @@
 		<style type="text/css"><?php include "./styles/connexion.css" ?></style>
 	</head>
 	<body>
-		<?php
-<<<<<<< HEAD
+<?php
 		session_start();
             if(isset($_GET["lien"])){
-=======
-            if(isset($_GET["lien"])){ //prend le lien de l'app pour retourner une page qui s'appelle "login"
->>>>>>> 513187f8fa8b2bdf4a92bf5a779e3f137a1cf9e1
                 $lien=$_GET["lien"];
                 switch($lien)
                 {
                     case"login":
                         include ("./pages/connexion.php");
-<<<<<<< HEAD
                         break;
                     case "evenement_rechercher":
                         include ("./pages/evenement_rechercher.php");
@@ -25,12 +20,9 @@
                     case "question":
                         include ("./pages/question.php");
                         break;
-=======
-                        break; 
                     case "evenement_liste":
                         include ("./pages/evenement_liste.php");
                         break;                
->>>>>>> 513187f8fa8b2bdf4a92bf5a779e3f137a1cf9e1
                 }
             }
             else
